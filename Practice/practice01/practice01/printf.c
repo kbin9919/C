edc55103d6ca76304878b62968ea97f0d3c37ed4
2KB\kbin9919 <osl71297264@gmail.com>
2025-09-10 21:42:21 +0900

    [수정] 주석 제거

--- Practice/practice01/practice01/printf.c
+++ Practice/practice01/practice01/printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-//int main() {
-//    int age = 25;
-//    float height = 175.5;
-//    char grade = 'A';
-//    char name[] = "Lee";
-//
-//    printf("이름: %s\n", name);       // 문자열 출력
-//    printf("나이: %d\n", age);        // 정수 출력
-//    printf("키: %.1fcm\n", height);   // 소수점 1자리 출력
-//    printf("성적: %c\n", grade);      // 문자 출력
-//
-//    return 0;
-//}
+int main() {
+    int age = 25;
+    float height = 175.5;
+    char grade = 'A';
+    char name[] = "Lee";
+
+    printf("이름: %s\n", name);       // 문자열 출력
+    printf("나이: %d\n", age);        // 정수 출력
+    printf("키: %.1fcm\n", height);   // 소수점 1자리 출력
+    printf("성적: %c\n", grade);      // 문자 출력
+
+    return 0;
+}
